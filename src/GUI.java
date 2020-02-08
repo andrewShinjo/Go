be82c7de5343e29c1e6455b0275a0996f830b8a5
andrewShinjo <shinjo1233@gmail.com>
2020-02-07 20:59:15 -0800

    Added image of board with buttons on each location

--- src/GUI.java
+++ src/GUI.java
@@ -27,26 +27,29 @@ public class GUI {
         // Set up components to create board
         board.setLayout(new GridLayout(19, 19));
 
-        // create all the JButtons
+        // create image of board in window
+        ImageIcon board_icon = new ImageIcon("baduk board.png");
+        Image board_image = board_icon.getImage();
+        Image rescaled_board_image = board_image.getScaledInstance(570, 570, java.awt.Image.SCALE_SMOOTH);
+        board_icon = new ImageIcon(rescaled_board_image);
+
+        JLabel board_label = new JLabel(board_icon);
+        board_label.setLayout(new GridLayout(BOARD_COL, BOARD_ROW));
+
         for (int i = 1; i <= BOARD_COL; i++) {
             for (int j = 1; j <= BOARD_ROW; j++) {
                 JButton button = new JButton();
-                button.setPreferredSize(new Dimension(30, 30));
-                board.add(button);
+                button.setMinimumSize(new Dimension(30, 30));
+                button.setMaximumSize((new Dimension(30, 30)));
+                button.setOpaque(false);
+                button.setContentAreaFilled(false);
+                button.setBorderPainted(false);
+                board_label.add(button);
             }
         }
 
-        JLabel image = new JLabel();
-        ImageIcon img = new ImageIcon("baduk board.png");
-        image.setIcon(img);
-
-        // Testing JLayeredPane
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.add(board, 2);
-        layeredPane.add(image, 1);
-
         GridBagConstraints c1 = new GridBagConstraints();
         c1.anchor = GridBagConstraints.CENTER;
-        window.add(layeredPane, c1);
+        window.add(board_label, c1);
     }
 }
